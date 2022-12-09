e305ac698c5a96315bd127d7f1d97517eb56b2ab
hieutn1 <hieutn1@vietis.com.vn>
2022-12-09 18:26:51 +0700

    add list product in tablayout

--- app/src/main/java/com/example/e_commerceshopping/ui/home/ListItemFragment.kt
+++ app/src/main/java/com/example/e_commerceshopping/ui/home/ListItemFragment.kt
@@ -3,14 +3,14 @@ package com.example.e_commerceshopping.ui.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.e_commerceshopping.base.ui.BaseFragment
-import com.example.e_commerceshopping.databinding.FragmentListItemBinding
+import com.example.e_commerceshopping.databinding.ItemProductBinding
 import com.example.e_commerceshopping.ui.MainActivity
 
-class ListItemFragment : BaseFragment<FragmentListItemBinding, MainActivity>(){
+class ListItemFragment : BaseFragment<ItemProductBinding, MainActivity>(){
     override fun onInflateView(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentListItemBinding = FragmentListItemBinding.inflate(inflater)
+    ): ItemProductBinding = ItemProductBinding.inflate(inflater)
 
     override fun setupView() {
 
